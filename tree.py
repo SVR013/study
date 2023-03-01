463757233faa600a6158a8cc90d6cb813b7a4ae3
SVR013 <oleg10114@gmail.com>
2023-03-01 18:40:52 +0200

    Добавил выбор операционной системы в tree.py
    После обновления в программе появилось меню с выбором
    операционной системы и программа работает одинаково на
    Linux и Windows

--- tree.py
+++ tree.py
@@ -1,14 +1,46 @@
 import os
 import os.path
 
-def DrawTree(path, template=''):
+def DrawTreeLinux(path, template=''):
     lst = sorted(os.listdir(path))
     for i in lst:
         flag = True if i == lst[-1] else False
         continuation_of_the_line = '└─> ' + i if flag else '├─> ' + i
         print(template + continuation_of_the_line)
         if os.path.isdir(path + '/' + i):
-            DrawTree(path + '/' + i, (template + (('    ') if flag else '│   ')))
+            DrawTreeLinux(path + '/' + i, (template + (('    ') if flag else '│   ')))
 
-DrawTree(os.getcwd())
+def DrawTreeWindows(path, template=''):
+    lst = sorted(os.listdir(path))
+    for i in lst:
+        flag = True if i == lst[-1] else False
+        continuation_of_the_line = '└─> ' + i if flag else '├─> ' + i
+        print(template + continuation_of_the_line)
+        if os.path.isdir(path + '\\' + i):
+            DrawTreeWindows(path + '\\' + i, (template + (('    ') if flag else '│   ')))
+
+def Run() -> str:
+    # выводит меню пользователю
+    commands = ['1', '2', '0']
+    print('Какая у вас операционная система? (укажите номер)\n',
+          '1. Linux\n', '2.Windows\n',
+          '0. Выход из программы')
+    command = input()
+    while True:
+        if command not in commands:
+            print('Укажите натуральное число от 0 до 2')
+            command = input()
+        else:
+            break
+    return command
+
+while True:
+    print()
+    command = Run()
+    if command == '1':
+        DrawTreeLinux(os.getcwd())
+    elif command == '2':
+        DrawTreeWindows(os.getcwd())
+    elif command == '0':
+        break
 
